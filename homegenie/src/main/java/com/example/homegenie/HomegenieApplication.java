0ccbe56064503ee167224b2cdbe6f514982af692
= <vishakhamangtani23@gmail.com>
2024-05-11 11:39:19 +0530

    intitial commit

--- homegenie/src/main/java/com/example/homegenie/HomegenieApplication.java
+++ homegenie/src/main/java/com/example/homegenie/HomegenieApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HomegenieApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HomegenieApplication.class, args);
+
+		System.out.println("Hello World");
+
+	SpringApplication.run(HomegenieApplication.class, args);
 	}
 
 }
