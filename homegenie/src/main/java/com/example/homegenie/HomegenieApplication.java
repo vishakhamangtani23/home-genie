9373184487419c453f639cf9e824864132aeeb76
= <vishakhamangtani23@gmail.com>
2024-05-11 11:27:35 +0530

    initial commit

---
+++ homegenie/src/main/java/com/example/homegenie/HomegenieApplication.java
@@ -0,0 +1,13 @@
+package com.example.homegenie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomegenieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomegenieApplication.class, args);
+	}
+
+}
---
+++ homegenie/src/test/java/com/example/homegenie/HomegenieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homegenie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomegenieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
