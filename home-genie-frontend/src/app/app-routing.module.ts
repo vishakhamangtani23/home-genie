e93e19454a6fe561a49355f5710903bffbd58aa2
Hirday26 <hirdayrochani@gmail.com>
2024-05-12 01:51:16 +0530

    add -work

--- home-genie-frontend/src/app/app-routing.module.ts
+++ home-genie-frontend/src/app/app-routing.module.ts
@@ -1,3 +1,84 @@
+// import { Component, NgModule } from '@angular/core';
+// import { RouterModule, Routes } from '@angular/router';
+// import { HeroComponent } from './home/hero/hero.component';
+// import { NavbarComponent } from './shared/navbar/navbar.component';
+// import { ResetPasswordComponent } from './auth/reset-password/reset-password.component';
+// import { ForgotPasswordComponent } from './auth/forgot-password/forgot-password.component';
+// import { LoginComponent } from './auth/login/login.component';
+// import { RegisterComponent } from './auth/register/register.component';
+// import { SuccessComponent } from './shared/success/success.component';
+// import { NotfoundComponent } from './shared/notfound/notfound.component';
+// import { ChatHelpComponent } from './home/chat-help/chat-help.component';
+// import { AddToCartComponent } from './home/add-to-cart/add-to-cart.component';
+// import { CategoriesComponent } from './home/categories/categories.component';
+// import { FetchUserComponent } from './auth/fetch-user/fetch-user.component';
+// import { BookingComponent } from './home/booking/booking.component';
+// // import { FetchUserComponent } from './auth/fetch-user/fetch-user.component';
+
+// const routes: Routes = [
+//   {
+//     path: '',
+//     component: RegisterComponent,
+//   },
+//   {
+//     path: 'home',
+//     component: HeroComponent,
+//   },
+//   {
+//     path: 'register',
+//     component: RegisterComponent,
+//   },
+//   {
+//     path: 'login',
+//     component: LoginComponent,
+//   },
+//   {
+//     path: 'chat',
+//     component: ChatHelpComponent,
+//   },
+//   {
+//     path: 'app-navbar',
+//     component: NavbarComponent,
+//   },
+//   {
+//     path: 'reset/:token',
+//     component: ResetPasswordComponent,
+//   },
+//   {
+//     path: 'forgot-password',
+//     component: ForgotPasswordComponent,
+//   },
+//   {
+//     path: 'success',
+//     component: SuccessComponent,
+//   },
+//   {
+//     path: 'notfound',
+//     component: NotfoundComponent,
+//   },
+//   {
+//     path: 'add-to-cart',
+//     component: AddToCartComponent,
+//   },
+//   {
+//     path: 'category/:serviceId',
+//     component: CategoriesComponent,
+//   },
+//   {
+//     path:"fetch-user",
+//     component:FetchUserComponent
+//   },{
+//     path: "booking",
+//     component:BookingComponent
+//   },
+
+// ];
+
+// @NgModule({
+//   imports: [RouterModule.forRoot(routes)],
+//   exports: [RouterModule],
+// })
+// export class AppRoutingModule {}
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeroComponent } from './home/hero/hero.component';
@@ -12,11 +93,8 @@ import { ChatHelpComponent } from './home/chat-help/chat-help.component';
 import { AddToCartComponent } from './home/add-to-cart/add-to-cart.component';
 import { CategoriesComponent } from './home/categories/categories.component';
 import { FetchUserComponent } from './auth/fetch-user/fetch-user.component';
-<<<<<<< HEAD
 import { AddWorkComponent } from './auth/add-work/add-work.component';
-=======
 import { BookingComponent } from './home/booking/booking.component';
->>>>>>> fcb2591325193e691248fbe89104e2cecf0b6378
 // import { FetchUserComponent } from './auth/fetch-user/fetch-user.component';
 
 const routes: Routes = [
@@ -71,17 +149,14 @@ const routes: Routes = [
   {
     path:"fetch-user",
     component:FetchUserComponent
-<<<<<<< HEAD
   },
   {
     path:"add-work",
     component:AddWorkComponent
-=======
   },{
     path: "booking",
     component:BookingComponent
->>>>>>> fcb2591325193e691248fbe89104e2cecf0b6378
-  },
+  }
 
 ];
 
