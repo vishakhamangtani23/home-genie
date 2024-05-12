c126973548c482f7a47d836a523fb80fb6dee06e
= <vishakhamangtani23@gmail.com>
2024-05-12 08:33:30 +0530

    9 am update

--- home-genie-frontend/src/app/home/home.module.ts
+++ home-genie-frontend/src/app/home/home.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeroComponent } from './hero/hero.component';
-import { ChatHelpComponent } from './chat-help/chat-help.component';
+// import { ChatHelpComponent } from './chat-help/chat-help.component';
 
 import { HttpClientModule } from '@angular/common/http';
 import { ProductsComponent } from './products/products.component';
@@ -19,7 +19,6 @@ import { BookingsListComponent } from './bookings-list/bookings-list.component';
 @NgModule({
   declarations: [
     HeroComponent,
-    ChatHelpComponent,
     ProductsComponent,
     ServicesComponent,
     RatingsComponent,
@@ -36,7 +35,6 @@ import { BookingsListComponent } from './bookings-list/bookings-list.component';
   ],
   exports: [
     HeroComponent,
-    ChatHelpComponent,
     ProductsComponent,
     ServicesComponent,
     RatingsComponent
